8f8b79a4fda5abf29fadbf39cdb328fc266fbc78
danila12315454 <danilaprig@gmail.com>
2024-05-25 20:44:42 +0200

    Part of work done v2

--- src/Containers/Container.java
+++ src/Containers/Container.java
@@ -1,10 +1,17 @@
 package Containers;
 
+import Items.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Container {
     private double height;
     private double width;
     private double length;
     private String type;
+    private double usedSpace;
+    private double usedWeight;
 
     public Container(String type, double height, double width, double length) {
         this.type = type;
